ff743db1646cf6533078dfb2bd419355cacbd19d
jesus2p <jesusperapa@gmail.com>
2026-02-02 10:07:34 +0100

    Update PracticaGitHub.java

--- src/PracticaGitHub.java
+++ src/PracticaGitHub.java
@@ -1,5 +1,6 @@
 public class PracticaGitHub {
     public static void main(String[] args) {
         System.out.println("Practica GitHub de Jesús Pérez");
+        System.out.println("Practica GitHub de Jesús Pérez editada desde GitHub");
     }
 }
